72f1519ab747aac1837364e426fd43e6e072b5f0
Martin Hardcastle <mjh@extragalactic.info>
2018-02-22 21:21:10 +0000

    more geometries and normalization

--- testsynchro.py
+++ testsynchro.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 c=FlatLambdaCDM(H0=70, Om0=0.3, Tcmb0=2.725)
 
-s=SynchSource(type='sphere',asph=5,z=0.1,cosmology=WMAP9,spectrum='broken',dpow=1,gbreak=1e3,gmin=10,gmax=1e5,injection=2.1,B=1e-9)
+s=SynchSource(type='sphere',asph=5,z=0.1,cosmology=WMAP9,spectrum='broken',dpow=1,gbreak=1e3,gmin=10,gmax=1e5,injection=3.1,B=1e-9,verbose=True)
 #s=SynchSource(type='cylinder',arcyl=10,alcyl=10,z=0.1,cosmology=c,spectrum='powerlaw',gmin=10,gmax=1e6,injection=2.0,B=1e-9,verbose=True)
 
 print s.volume
